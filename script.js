d22f43d00ff0330ea1a056e691574a47869b0298
Shaurya-2005 <shaurya123rai@gmail.com>
2024-11-01 18:03:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none'; // Hide all sections
+    });
+    document.getElementById(sectionId).style.display = 'block'; // Show selected section
+}
+
+// Show the home section by default on page load
+window.onload = function() {
+    showSection('home');
+}
